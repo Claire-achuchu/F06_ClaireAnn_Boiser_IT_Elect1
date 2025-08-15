5b57bbf8df42dc7b2806f3e92258e10261e2b9ec
Claire-achuchu <bubblessssss2025@gmail.com>
2025-08-15 17:22:05 +0800

    Counter App

---
+++ doghat.js
@@ -0,0 +1,17 @@
+import React from ’ react ’;
+import { View , Text } from ’ react - native ’;
+
+const Greeting = () = > {
+
+return (
+
+<View >
+
+< Text > Hello , React Native ! </ Text >
+
+</ View >
+
+) ;
+ };
+
+export default Greeting ;
